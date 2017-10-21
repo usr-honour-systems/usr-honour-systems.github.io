c5d4e50fd1f4cd6d106ddd2c0e22e599a216e249
Yui Wei <tyuiwei@gmail.com>
2017-10-21 18:02:46 +0800

    test

---
+++ js/carousel.js
@@ -0,0 +1,7 @@
+$('.carousel-control-prev').click(function() {
+  $('#myCarousel').carousel('prev');
+});
+
+$('.carousel-control-next').click(function() {
+  $('#myCarousel').carousel('next');
+});
